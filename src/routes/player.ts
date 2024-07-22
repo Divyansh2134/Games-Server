aa8e2e8e82bbda4fd2d043fcb92b1c015ecd86cb
Divyansh2134 <divyanshsharma2134@gmail.com>
2024-07-22 09:29:03 +0530

    player shema

---
+++ src/routes/player.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import Players from '../models/players';
+
+const router = Router();
+
+
+router.post('/', async (req, res) => {
+  const { name, position, team, age } = req.body;
+
+  try {
+    const player = new Players({
+      name,
+      position,
+      team,
+      age
+    });
+
+    await player.save();
+
+    res.json(player);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    } else {
+      console.error('Unexpected error', err);
+      res.status(500).send('Server Error');
+    }
+  }
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import connectDB from './db';
+import playerRoutes from './routes/player';
 
 const app = express();
 const port = 3000;
+
+connectDB();
+
+app.use('/api/players', playerRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
